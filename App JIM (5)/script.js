0463f129e417f40b3dbaac171b2ed8b7668402a3
AppJIM2024 <appjim123@gmail.com>
2024-07-05 17:43:59 -0300

    Delete App JIM (5) directory

--- App JIM (5)/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('toggleBtn').addEventListener('click', function() {
-    // Alternar a classe 'collapsed' na barra lateral
-    document.getElementById('sidebar').classList.toggle('collapsed');
-    document.getElementById('main').classList.toggle('collapsed');
-});
